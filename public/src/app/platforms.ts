620004d06363e0b95897d1645a5fc5a7be2d02a3
Marc Durdin <marc@durdin.net>
2021-03-24 16:28:18 +1100

    fix: stable build reporting

--- public/src/app/platforms.ts
+++ public/src/app/platforms.ts
@@ -12,43 +12,43 @@ export const platforms: PlatformSpec[] = [
   {
     id:'android',
     name:'Android',
-    configs:{"alpha": "KeymanAndroid_Build", "beta": "KeymanAndroid_Build", "stable": "KeymanAndroid_Stable", test: "KeymanAndroid_TestPullRequests"},
+    configs:{"alpha": "KeymanAndroid_Build", "beta": "KeymanAndroid_Build", "stable": "KeymanAndroid_Build", test: "KeymanAndroid_TestPullRequests"},
     context: "Keyman - Android"
   },
   {
     id:'ios',
     name:'iOS',
-    configs:{"alpha": "Keyman_iOS_Master", "beta": "Keyman_iOS_Master", "stable": "Keyman_iOS_Stable", test: "Keyman_iOS_TestPullRequests"},
+    configs:{"alpha": "Keyman_iOS_Master", "beta": "Keyman_iOS_Master", "stable": "Keyman_iOS_Master", test: "Keyman_iOS_TestPullRequests"},
     context: "Keyman - iOS"
   },
   {
     id:'linux',
     name:'Linux',
-    configs:{"alpha": "KeymanLinux_Master", "beta": "KeymanLinux_Master", "stable": "KeymanLinux_Stable", test: "KeymanLinux_TestPullRequests"},
+    configs:{"alpha": "KeymanLinux_Master", "beta": "KeymanLinux_Master", "stable": "KeymanLinux_Master", test: "KeymanLinux_TestPullRequests"},
     context: "Keyman - Linux"
   },
   {
     id:'mac',
     name:'macOS',
-    configs:{"alpha": "KeymanMac_Master", "beta": "KeymanMac_Master", "stable": "KeymanMac_Stable", test: "Keyman_KeymanMac_PullRequests"},
+    configs:{"alpha": "KeymanMac_Master", "beta": "KeymanMac_Master", "stable": "KeymanMac_Master", test: "Keyman_KeymanMac_PullRequests"},
     context: "Keyman - macOS"
   },
   {
     id:'web',
     name:'KeymanWeb',
-    configs:{"alpha": "Keymanweb_Build", "beta": "Keymanweb_Build", "stable": "Keymanweb_Stable", test: "Keymanweb_TestPullRequests"},
+    configs:{"alpha": "Keymanweb_Build", "beta": "Keymanweb_Build", "stable": "Keymanweb_Build", test: "Keymanweb_TestPullRequests"},
     context: "Keyman - Web"
   },
   {
     id:'windows',
     name:'Windows',
-    configs:{"alpha": "Keyman_Build", "beta": "Keyman_Build", "stable": "KeymanDesktop_Stable", test: "KeymanDesktop_TestPullRequests"},
+    configs:{"alpha": "Keyman_Build", "beta": "Keyman_Build", "stable": "Keyman_Build", test: "KeymanDesktop_TestPullRequests"},
     context: "Keyman - Windows \\(Desktop\\/Developer\\)"
   },
   {
     id:'developer',
     name:'Developer',
-    configs:{"alpha": "Keyman_Build", "beta": "Keyman_Build", "stable": "KeymanDesktop_Stable", test: "KeymanDesktop_TestPullRequests"},
+    configs:{"alpha": "Keyman_Build", "beta": "Keyman_Build", "stable": "Keyman_Build", test: "KeymanDesktop_TestPullRequests"},
     context: "Keyman - Windows \\(Desktop\\/Developer\\)"
   },
   {

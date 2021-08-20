69b307d11ebd1af844e79a94ccae8d42c6214867
Marc Durdin <marc@durdin.net>
2021-08-20 12:32:56 +1000

    chore: cleanup

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,37 +1,8 @@
-import { NgZone, Component, PlatformRef } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { StatusService } from './status/status.service';
-import { StatusSource } from '../../../shared/status-source';
-import { platforms, PlatformSpec } from './platforms';
-import { sites, siteSentryNames } from './sites';
-import { repoShortNameFromGithubUrl } from './utility/repoShortNameFromGithubUrl';
-import { escapeHtml } from './utility/escapeHtml';
-import { DataSocket } from './datasocket/datasocket.service';
-import emojiRegex from 'emoji-regex/es2015/RGI_Emoji';
-
-interface Status {
-  currentSprint: any;
-  github: any;
-  issues: any;
-  contributions: any;
-  keyman: any[];
-  sentryIssues: any;
-  teamCity: any[];
-  teamCityRunning: any[];
-  deployment: {
-  }
-};
-
-interface OtherSites {
-  repos: string[];
-  pulls: any[];
-  milestones: any[];
-}
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  providers: [ StatusService ],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {

c4bcb9a27216cf02b2176041775dec6da89dcc09
Marc Durdin <marc@durdin.net>
2021-08-20 12:24:41 +1000

    feat: add routes and reorganise
    Add a route for manual-test, and reorganise the home page into a
    HomeComponent.

---
+++ public/src/app/manual-test/manual-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manual-test',
+  templateUrl: './manual-test.component.html',
+  styleUrls: ['./manual-test.component.css']
+})
+export class ManualTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

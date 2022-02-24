ae6ace37f725b20a12e995923e97dccb2e7513cb
Marc Durdin <marc@durdin.net>
2022-02-25 09:04:21 +1100

    chore: remove temp type stubs for Clipboard

--- public/src/app/clipboard/clipboard.component.ts
+++ public/src/app/clipboard/clipboard.component.ts
@@ -1,14 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
 
-// ClipboardItem and Clipboard.write are not yet in current types.d.ts
-declare class ClipboardItem {
-  constructor(data: { [mimeType: string]: Blob });
-}
-
-declare class Clipboard extends globalThis.Clipboard {
-  write(items: [ClipboardItem]);
-}
-
 @Component({
   selector: 'app-clipboard',
   templateUrl: './clipboard.component.html',
@@ -28,8 +19,8 @@ export class ClipboardComponent implements OnInit {
     const clipText =
       typeof this.text == 'function' ? this.text() : this.text;
     if(typeof clipText == 'object') {
-      let items: globalThis.ClipboardItems = [];
-      let item = new globalThis.ClipboardItem({[clipText.type]: new Blob([clipText.content], {type: clipText.type}) });
+      let items: ClipboardItems = [];
+      let item = new ClipboardItem({[clipText.type]: new Blob([clipText.content], {type: clipText.type}) });
       items.push(item);
       navigator.clipboard.write(items);
     } else {

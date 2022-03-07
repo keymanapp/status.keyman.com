513551a7c9d4c54d4e7fe239ae8c4028d08061e1
Marc Durdin <marc@durdin.net>
2022-03-07 14:37:04 +1100

    feat: spinner for copy-to-clipboard

--- public/src/app/clipboard/clipboard.component.ts
+++ public/src/app/clipboard/clipboard.component.ts
@@ -24,12 +24,19 @@ export class ClipboardComponent implements OnInit {
   @Input() text: any;
   @Input() title: any;
 
+  loading: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  finishCopying() {
+    this.loading = false;
+  }
+
   copyToClipboard() {
+    this.loading = true;
     // async but we don't need to wait around for the answer
     const clipText =
       typeof this.text == 'function' ? this.text() : this.text;
@@ -37,9 +44,9 @@ export class ClipboardComponent implements OnInit {
       let items: ClipboardItems = [];
       let item = new ClipboardItem({[clipText.type]: new Blob([clipText.content], {type: clipText.type}) });
       items.push(item);
-      navigator.clipboard.write(items);
+      navigator.clipboard.write(items).then(this.finishCopying.bind(this));
     } else {
-      navigator.clipboard.writeText(clipText);
+      navigator.clipboard.writeText(clipText).then(this.finishCopying.bind(this));
     }
   }
 }

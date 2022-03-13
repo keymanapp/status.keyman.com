4718bbcae52ce3ba7df08e3b488877f3618a6af0
Marc Durdin <marc@durdin.net>
2022-03-14 07:13:45 +1100

    feat: add keyboards link

--- shared/artifact-links.ts
+++ shared/artifact-links.ts
@@ -23,6 +23,7 @@ export const artifactLinks = {
     'KeymanDesktop_TestPullRequests': {platform: 'windows', name: 'Windows', icon: 'windows.png', downloads: [
       {fragment: 'release/$version/keyman-$version.exe', name: 'Keyman for Windows', icon: 'keyman.png'} ,
       {fragment: 'release/$version/keymandeveloper-$version.exe', name: 'Keyman Developer', icon: 'developer.png'} ,
+      {fragment: 'keyboards/index.html', name: 'Test Keyboards', icon: 'developer.png'} ,
       {fragment: 'release/$version/firstvoices-$version.exe', name: 'FirstVoices Keyboards for Windows', icon: 'firstvoices.png'} ,
     ]},
 

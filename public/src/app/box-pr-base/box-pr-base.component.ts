82d93a9ec61a4bebf20201d91a1a67959627b79c
Marc Durdin <marc@durdin.net>
2022-09-12 15:07:40 +1000

    feat: ultimate base branch

--- public/src/app/box-pr-base/box-pr-base.component.ts
+++ public/src/app/box-pr-base/box-pr-base.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-box-pr-base',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./box-pr-base.component.css']
 })
 export class BoxPrBaseComponent implements OnInit {
+  @Input() pullsByBase: any;
+  @Input() changeCounter: number;
+  @Input() status: any;
 
   constructor() { }
 

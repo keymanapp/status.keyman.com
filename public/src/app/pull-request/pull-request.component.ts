7e98f96c3cdab9ee35dee123dffe14461abe4c92
Darcy Wong <darcy_wong@sil.org>
2023-04-21 09:19:39 +0700

    chore: Include 16.0 links for now

--- public/src/app/pull-request/pull-request.component.ts
+++ public/src/app/pull-request/pull-request.component.ts
@@ -112,6 +112,10 @@ export class PullRequestComponent extends PopupComponent implements OnInit, OnCh
     'KeymanAndroid_TestPullRequests': {platform: 'android', name: 'Android', icon: 'android.png', downloads: [
       {fragment: 'release/keyman-$version.apk', name: 'Keyman for Android apk', icon: 'keyman.png'} ,
       {fragment: 'release/FirstVoices/firstvoices-$version.apk', name: 'FirstVoices Keyboards for Android apk', icon: 'firstvoices.png'} ,
+
+      // TODO: Remove 16.0 links when 17.0 released
+      {fragment: 'release/kMAPro-debug.apk', name: 'Keyman for Android 16.0 apk', icon: 'keyman.png'} ,
+      {fragment: 'release/FirstVoices/app-debug.apk', name: 'FirstVoices for Android 16.0 apk', icon: 'firstvoices.png'} ,
     ]},
     'KeymanAndroid_TestSamplesAndTestProjects': {platform: 'android', name: 'Android', icon: 'android.png', downloads: [
       {fragment: 'Samples/KMSample1/app-debug.apk', name: 'KMSample1 apk', icon: 'kmsample1.png'} ,

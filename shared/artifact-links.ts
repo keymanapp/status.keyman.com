c821d7a13750782804a5b83f2eea990face5da78
Marc Durdin <marc@durdin.net>
2026-07-14 13:33:56 +0200

    chore(android): update Android artifact links and remove legacy jenkins links
    Also DRY teamCityTargets
    
    Test-bot: skip

--- shared/artifact-links.ts
+++ shared/artifact-links.ts
@@ -1,32 +1,28 @@
-// TODO: update pull-request.component.ts to use these
-// TODO: jenkins links
+/*
+ * Keyman is copyright (C) SIL Global. MIT License.
+ */
+
 export const artifactLinks = {
   teamCityTargets: {
     'KeymanAndroid_TestPullRequests': {platform: 'android', name: 'Android', icon: 'android.png', downloads: [
-      {fragment: 'release/keyman-$version_with_tag.apk', name: '**Keyman for Android** apk', icon: 'keyman.png'} ,
+      {fragment: 'release/keyman-$version_with_tag.apk', name: '**Keyman for Android** apk', icon: 'keyman.png'},
       {fragment: 'release/FirstVoices/firstvoices-$version_with_tag.apk', name: 'FirstVoices Keyboards for Android apk', icon: 'firstvoices.png'} ,
-      {fragment: 'release/keyman-$version.apk', name: 'Keyman for Android apk (old PRs)', icon: 'keyman.png'} ,
-      {fragment: 'release/FirstVoices/firstvoices-$version.apk', name: 'FirstVoices Keyboards for Android apk (old PRs)', icon: 'firstvoices.png'} ,
     ]},
     'KeymanAndroid_TestSamplesAndTestProjects': {platform: 'android', name: 'Android', icon: 'android.png', downloads: [
-      {fragment: 'Samples/KMSample1/app-debug.apk', name: 'KMSample1 apk', icon: 'kmsample1.png'} ,
-      {fragment: 'Samples/KMSample2/app-debug.apk', name: 'KMSample2 apk', icon: 'kmsample2.png'} ,
-      {fragment: 'Tests/KeyboardHarness/app-debug.apk', name: 'KeyboardHarness apk', icon: 'keyboardharness.png'} ,
+      // We always run the debug builds for these projects
+      {fragment: 'Samples/KMSample1/debug/KMSample1-$version_with_tag-debug.apk', name: 'KMSample1 apk', icon: 'kmsample1.png'} ,
+      {fragment: 'Samples/KMSample2/debug/KMSample2-$version_with_tag-debug.apk', name: 'KMSample2 apk', icon: 'kmsample2.png'} ,
+      {fragment: 'Tests/KeyboardHarness/debug/KeyboardHarness-$version_with_tag-debug.apk', name: 'KeyboardHarness apk', icon: 'keyboardharness.png'} ,
     ]},
 
     'Keyman_iOS_TestPullRequests': {platform: 'ios', name: 'iOS', icon: 'ios.png', downloads: [
       {fragment: 'upload/$version/keyman-ios-simulator-$version_with_tag.app.zip', name: '**Keyman for iOS** (simulator image)', icon: 'keyman.png'} ,
       {fragment: 'upload/$version/firstvoices-ios-simulator-$version_with_tag.app.zip', name: 'FirstVoices Keyboards for iOS (simulator image)', icon: 'firstvoices.png'} ,
-      //TODO: remove next two downloads when version_with_tag artifact filenames have propagated everywhere
-      {fragment: 'upload/$version/keyman-ios-simulator-$version.app.zip', name: 'Keyman for iOS (simulator image) (old PRs)', icon: 'keyman.png'} ,
-      {fragment: 'upload/$version/firstvoices-ios-simulator-$version.app.zip', name: 'FirstVoices Keyboards for iOS (simulator image) (old PRs)', icon: 'firstvoices.png'} ,
     ]},
 
     'Keyman_KeymanMac_PullRequests': {platform: 'mac', name: 'macOS', icon: 'mac.png', downloads: [
       {fragment: 'upload/$version/keyman-$version_with_tag.dmg', name: '**Keyman for macOS (.dmg)**', icon: 'keyman.png'} ,
       {fragment: 'upload/$version/keyman-$version_with_tag.pkg', name: '**Keyman for macOS (.pkg)**', icon: 'keyman.png'} ,
-      //TODO: remove next download when version_with_tag artifact filenames have propagated everywhere
-      {fragment: 'upload/$version/keyman-$version.dmg', name: 'Keyman for macOS (old PRs)', icon: 'keyman.png'} ,
     ]},
 
     'KeymanDesktop_TestPullRequests': {platform: 'windows', name: 'Windows', icon: 'windows.png', downloads: [
@@ -35,9 +31,6 @@ export const artifactLinks = {
       {fragment: 'support/editor32.exe', name: 'Text Editor (x86)', icon: 'tool.png'} ,
       {fragment: 'support/editor64.exe', name: 'Text Editor (x64)', icon: 'tool.png'} ,
       {fragment: 'support/editorarm64.exe', name: 'Text Editor (ARM64)', icon: 'tool.png'} ,
-      //TODO: remove next two downloads when version_with_tag artifact filenames have propagated everywhere
-      {fragment: 'release/$version/keyman-$version.exe', name: 'Keyman for Windows (old PRs)', icon: 'keyman.png'} ,
-      {fragment: 'release/$version/firstvoices-$version.exe', name: 'FirstVoices Keyboards for Windows (old PRs)', icon: 'firstvoices.png'} ,
     ]},
 
     'Keyman_Developer_Test': {platform: 'developer', name: 'Developer', icon: 'developer.png', downloads: [
@@ -45,9 +38,6 @@ export const artifactLinks = {
       {fragment: 'release/$version/kmcomp-$version_with_tag.zip', name: 'kmcomp.zip', icon: 'developer.png'},
       {fragment: 'keyboard-regression/', name: 'Compiler Regression Tests', icon: 'developer.png'},
       {fragment: 'keyboards/index.html', name: 'Test Keyboards', icon: 'developer.png'} ,
-      //TODO: remove next two downloads when version_with_tag artifact filenames have propagated everywhere
-      {fragment: 'release/$version/keymandeveloper-$version.exe', name: 'Keyman Developer (old PRs)', icon: 'developer.png'} ,
-      {fragment: 'release/$version/kmcomp-$version.zip', name: 'kmcomp.zip (old PRs)', icon: 'developer.png'},
     ]},
 
     'Keymanweb_TestPullRequests': {platform: 'web', name: 'Web', icon: 'web.png', downloads: [
@@ -55,9 +45,4 @@ export const artifactLinks = {
     ]},
   },
 
-  jenkinsTarget: {
-    platform: 'linux', name: 'linux', icon: 'linux.png', downloads: [
-      { fragment: 'artifact/*zip*/archive.zip', name: 'Keyman for Linux', icon: 'keyman.png' }
-    ]
-  }
 }
